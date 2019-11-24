209ff0b51419a60e68c32680f02d21d91361dd43
Dan Moneypenny <catullus1973@gmail.com>
2019-11-24 15:54:01 -0800

    create placeholder App.js

--- applications/portfolio/src/App.js
+++ applications/portfolio/src/App.js
@@ -1,72 +1,11 @@
-import React, { useEffect, useContext } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from 'react';
 
-// context
-import { ViewContext } from './contexts/ViewContext';
-
-// routes
-//import { routes } from './routes/routes';
-
-// layout
-import Container from './components/grid/Container';
-import Header from './components/page/Header';
-import NavMain from './components/page/Nav/NavMain';
-import Main from './components/page/Main';
-
-// views
-import Home from './components/views/Home';
-import About from './components/views/About';
-import Work from './components/views/Work/Work';
-import Contact from './components/views/Contact';
-import Terms from './components/views/Terms';
-import Seed from './components/views/Seed';
 
 
 const App = () => {
 
-	const { docTitle } = useContext(ViewContext);
-	
-	useEffect(() => {
-		document.title = docTitle;
-	},[docTitle]);
-
 	return (
-		<Container>
-			<Header/>
-			<NavMain/>
-			<Main>
-				<Switch>
-					<Route
-						path='/'
-						exact
-						component={Home}
-					/>
-					<Route
-						path='/about'
-						component={About}
-					/>
-					<Route
-						path='/work'
-						component={Work}
-					/>
-												
-					<Route
-						path='/contact'
-						component={Contact}
-					/>
-					<Route
-						path='/terms'
-						exact
-						component={Terms}
-					/>
-					<Route
-						path='/seed'
-						exact
-						component={Seed}
-					/>																							
-				</Switch>
-			</Main>
-		</Container>
+		<h4 style={{ textAlign: "center", marginTop: "50px"}}>Coming soon...</h4>
 	);
 }
 
--- applications/portfolio/src/index.js
+++ applications/portfolio/src/index.js
@@ -3,13 +3,4 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ViewProvider } from './contexts/ViewContext';
-
-ReactDOM.render(
-<ViewProvider>
-  <Router>
-    <App/>
-  </Router>
-</ViewProvider>, 
-document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
